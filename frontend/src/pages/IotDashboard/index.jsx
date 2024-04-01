de557ada904761df6d36b42276822a0e35cb7b83
Khoa LTN <vietnamkhoale@gmail.com>
2024-04-01 11:49:38 +0700

    chore: display SensorList on IotDashboard

--- frontend/src/pages/IotDashboard/index.jsx
+++ frontend/src/pages/IotDashboard/index.jsx
@@ -1,12 +1,13 @@
 import PageLayout from "layouts/PageLayout";
 import DeviceList from "components/DeviceList";
+import SensorList from "components/SensorList";
 import { Card } from "@mui/joy";
 
 const IotDashboard = () => {
     return (
         <PageLayout focusOnRouteID={"home"}>
             <DeviceList />
-            <Card></Card>
+            <SensorList />
             <Card></Card>
             <Card></Card>
             <Card></Card>
