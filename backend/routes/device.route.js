7877bec37b1da42aaf5c8f209e35b6b02b6ad62b
Thu Nguyen Quoc Minh <thunguyenquocminh@192.168.2.2>
2024-04-01 10:34:04 +0700

    adjust avoid error

--- backend/routes/device.route.js
+++ backend/routes/device.route.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/auth.middleware');
 
 
 
-router.get('/',  deviceController.GetDevice);
+router.get('/', deviceController.GetDevice);
 router.get('/:id',  deviceController.GetDeviceById);
 router.post('/', deviceController.CreateDevice);
 router.put('/:id',  deviceController.UpdateDevice);
