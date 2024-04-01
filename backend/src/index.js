8c7fb813200f2fb9c600d1141f2391d92ad58f37
dvt <dvthanh19@gmail.com>
2024-04-01 07:57:35 +0700

    mổn modify

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,13 +37,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-
-app.use(routes)
+app.get('/', (req, res) => {
+    return res.send('INDEX PAGE');
+})
+// app.use(routes)
 // routes(app)
 // app.get('/', (req, res) => {
 //     return res.send('INDEX PAGE');
 // })
 
 
-// app.listen(port, () => console.log('Listening on http://localhost:' + port))
-app.listen(process.env.APP_PORT, () => console.log('Server up and running ...'));
+app.listen(port, () => console.log('Listening on http://localhost:' + port))
+// app.listen(process.env.APP_PORT, () => console.log('Listening on http://localhost:' + port));
