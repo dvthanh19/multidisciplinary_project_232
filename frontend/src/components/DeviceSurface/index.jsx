152e96c0a4b896124e19a7894d83482c2a851d6f
Khoa LTN <vietnamkhoale@gmail.com>
2024-04-02 20:46:33 +0700

    feat: enhance UI on DeviceSurface and DetailDevice

--- frontend/src/components/DeviceSurface/index.jsx
+++ frontend/src/components/DeviceSurface/index.jsx
@@ -1,24 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Card, Stack, Typography, Box, IconButton } from "@mui/joy";
+import { Card, Stack, Typography, Box, IconButton, Chip } from "@mui/joy";
 import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import InfoIcon from "@mui/icons-material/Info";
 import DeviceDetailModal from "components/DetailDevice/DeviceDetailModal";
-import { Line } from 'react-chartjs-2';
-import 'chart.js/auto';
+import { Line } from "react-chartjs-2";
+import "chart.js/auto";
 
-const DeviceSurface = ({
-    id,
-    deviceID,
-    name,
-    type,
-    position,
-}) => {
+const DeviceSurface = ({ id, deviceID, name, type, position }) => {
     const [deviceOn, setDeviceOn] = useState(true);
     const [openDetailModal, setOpenDetailModal] = useState(false);
     const [logData, setLogData] = useState([]);
-    const ADAFRUIT_IO_USERNAME = '1zy';
-    const ADAFRUIT_IO_KEY = 'aio_HQHl865UcZU9BnFNjemUKCfwh7Vx';
+    const ADAFRUIT_IO_USERNAME = "1zy";
+    const ADAFRUIT_IO_KEY = "aio_HQHl865UcZU9BnFNjemUKCfwh7Vx";
 
     const handleOpenDetailModal = () => {
         setOpenDetailModal(true);
@@ -26,11 +20,14 @@ const DeviceSurface = ({
 
     useEffect(() => {
         const fetchLogData = async () => {
-            const response = await fetch(`https://io.adafruit.com/api/v2/${ADAFRUIT_IO_USERNAME}/feeds/${deviceID}/data`, {
-                headers: {
-                    'X-AIO-Key': ADAFRUIT_IO_KEY,
+            const response = await fetch(
+                `https://io.adafruit.com/api/v2/${ADAFRUIT_IO_USERNAME}/feeds/${deviceID}/data`,
+                {
+                    headers: {
+                        "X-AIO-Key": ADAFRUIT_IO_KEY,
+                    },
                 }
-            });
+            );
             const data = await response.json();
             setLogData(data);
         };
@@ -39,14 +36,18 @@ const DeviceSurface = ({
     }, [name]);
 
     const data = {
-        labels: logData.map(log => new Date(log.created_at).toLocaleTimeString()),
-        datasets: [{
-            label: 'Value',
-            data: logData.map(log => log.value),
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1
-        }]
+        labels: logData.map((log) =>
+            new Date(log.created_at).toLocaleTimeString()
+        ),
+        datasets: [
+            {
+                label: "Value",
+                data: logData.map((log) => log.value),
+                fill: false,
+                borderColor: "rgb(75, 192, 192)",
+                tension: 0.1,
+            },
+        ],
     };
 
     return (
@@ -58,9 +59,9 @@ const DeviceSurface = ({
                 minWidth: 400,
                 minHeight: 300,
                 maxHeight: 300,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
             }}
         >
             <Stack direction="column" spacing={4}>
@@ -79,7 +80,17 @@ const DeviceSurface = ({
                         <PowerSettingsNewIcon />
                     </IconButton>
                     <Box maxWidth={300}>
-                        <Typography level="h3" noWrap>{name} ({type})</Typography>
+                        <Typography
+                            level="h3"
+                            endDecorator={
+                                <Chip color="success" variant="soft">
+                                    {type}
+                                </Chip>
+                            }
+                            noWrap
+                        >
+                            {name}
+                        </Typography>
                         <Typography
                             level="body-sm"
                             startDecorator={<LocationOnIcon />}
@@ -100,7 +111,11 @@ const DeviceSurface = ({
                 {/* Graph */}
                 <Line data={data} />
             </Stack>
-            <DeviceDetailModal open={openDetailModal} onClose={() => setOpenDetailModal(false)} deviceId={id} />
+            <DeviceDetailModal
+                open={openDetailModal}
+                onClose={() => setOpenDetailModal(false)}
+                deviceId={id}
+            />
         </Card>
     );
 };
