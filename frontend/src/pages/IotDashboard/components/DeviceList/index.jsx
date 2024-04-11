f83143ba4ba886809935f99fda612bf99b525906
Khoa LTN <vietnamkhoale@gmail.com>
2024-04-11 19:10:39 +0700

    feat: implement pagination on DeviceList

--- frontend/src/pages/IotDashboard/components/DeviceList/index.jsx
+++ frontend/src/pages/IotDashboard/components/DeviceList/index.jsx
@@ -1,15 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Stack, Chip, Box, Typography } from "@mui/joy";
+import { Stack, Chip, Box, Typography, CircularProgress } from "@mui/joy";
+import { Pagination, Fade } from "@mui/material";
 import DoneIcon from "@mui/icons-material/Done";
 import SensorsOffIcon from "@mui/icons-material/SensorsOff";
 import DeviceSurface from "../DeviceSurface";
 
-const DeviceList = ({type}) => {
+const devicesPerPage = 2;
+
+const groupDevicesToPages = (devices, devicesPerPage = 2) => {
+    // devices as object
+    // devicesPerPage is an integer
+    return devices.reduce((acc, element, index) => {
+        const currentGroup = Math.floor(index / devicesPerPage);
+        if (!acc[currentGroup]) {
+            acc[currentGroup] = [];
+        }
+        acc[currentGroup].push(element);
+        return acc;
+    }, {});
+};
+
+const filterDeviceTypes = (devices, type) => {
+    return devices.filter((device) => device.type === type);
+};
+
+const LoadingList = () => {
+    return (
+        <Stack
+            p={2}
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <Stack direction="column">
+                <Typography align="center">
+                    <CircularProgress size="lg" />
+                </Typography>
+                <Typography>Your devices are being prepared...</Typography>
+            </Stack>
+        </Stack>
+    );
+};
+
+const DeviceList = ({ type }) => {
     // deviceType can be {"Device", "Sensor", ...}
     const [devices, setDevices] = useState([]);
     const [numberOfDevice, setNumberOfDevices] = useState(0);
+    const [itemGroups, setItemGroups] = useState({});
     const [activeDevices, setActiveDevices] = useState(0);
+    const [page, setPage] = useState(1);
+    const [triggerFadeAnimation, setFadeAnimation] = useState(true);
 
     useEffect(() => {
         const fetchDevices = async () => {
@@ -17,37 +58,32 @@ const DeviceList = ({type}) => {
                 const response = await axios.get(
                     "http://localhost:3000/api/device/"
                 );
-                setDevices(response.data.device);
+                const devices = filterDeviceTypes(response.data.device, type);
+
                 // Assuming active is determined by curValue presence
+                setDevices(filterDeviceTypes(devices, type));
                 setActiveDevices(
-                    response.data.device.filter(
-                        (device) => device.curValue && device.type === type
-                    ).length
+                    devices.filter((device) => device.curValue).length
                 );
-                setNumberOfDevices(
-                    response.data.device.filter(
-                        (device) => device.type === type
-                    ).length
-                );
+                setNumberOfDevices(devices.length);
+                setItemGroups(groupDevicesToPages(devices, devicesPerPage));
             } catch (error) {
                 console.error("Failed to fetch devices:", error);
             }
         };
 
         fetchDevices();
+        localStorage.setItem("itemGroups", itemGroups);
     }, []);
 
-    // Warning:
-    // Putting both DeviceList and SensorList in one component can be difficult
-    // to ensure consistency over future components in IotDashboard.
-    //
-    // Needs future refactor.
+    useEffect(() => {}, [page]);
+
     return (
         <Stack spacing={6} direction="column">
             <Stack spacing={1} direction="column">
                 <Stack spacing={1} direction="row">
                     <Typography sx={{ color: "neutral.500" }}>
-                        Available sensors
+                        Available {type}s
                     </Typography>
                     <Chip
                         variant="outlined"
@@ -64,24 +100,40 @@ const DeviceList = ({type}) => {
                         Unactive {numberOfDevice - activeDevices}
                     </Chip>
                 </Stack>
-                <Stack
-                    spacing={2}
-                    direction="row"
-                    flexWrap="nowrap"
-                    sx={{ overflow: "auto" }}
-                >
-                    {devices
-                        .filter((device) => device.type === type)
-                        .map((device) => (
-                            <DeviceSurface
-                                id={device._id}
-                                deviceID={device.deviceID}
-                                name={device.name}
-                                type={device.type}
-                                position={device.position}
-                            />
-                        ))}
-                </Stack>
+
+                {Object.keys(itemGroups).length > 0 ? (
+                    <Fade in={true} timeout={1000}>
+                        <Stack
+                            spacing={2}
+                            direction="row"
+                            flexWrap="nowrap"
+                            sx={{ overflow: "auto" }}
+                        >
+                            {itemGroups[page - 1]
+                                .filter((device) => device.type === type)
+                                .map((device, idx) => (
+                                    <DeviceSurface
+                                        id={device._id}
+                                        deviceID={device.deviceID}
+                                        name={device.name}
+                                        type={device.type}
+                                        position={device.position}
+                                    />
+                                ))}
+                        </Stack>
+                    </Fade>
+                ) : (
+                    <LoadingList />
+                )}
+
+                <Pagination
+                    page={page}
+                    count={Object.keys(itemGroups).length}
+                    color="primary"
+                    boundaryCount={1}
+                    siblingCount={0}
+                    onChange={(event, page) => setPage(page)}
+                />
             </Stack>
         </Stack>
     );
